e66b2f5156aa10c16a2ac4fa384ec98aa718afd6
BetaBlaze <kostyushin996@gmail.com>
2023-07-06 22:18:13 +0300

    add openai provider with llama.cpp

---
+++ backend/llama/open_ai_provider.py
@@ -0,0 +1,70 @@
+import subprocess
+import openai
+import os
+openai.api_key = "betablaze-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # can be anything
+openai.api_base = "http://localhost:8000/v1"
+import json
+
+
+def get_llama_response(question, model_path, current_bot_personality, chat_history=[]):
+    bot_data = {
+        "bot_personality": current_bot_personality
+    }
+
+
+    # temp solution to handle chat history, will need to look into a better long term memory solution.
+    chat_history_string = ""
+    if len(chat_history) >= 1:
+        for message_and_response in chat_history:
+            chat_history_string += message_and_response["user_message"]
+            chat_history_string += message_and_response["bot_message"]
+
+    # using this lets us have an easy queue system for questions.
+    response = openai.Completion.create(
+        model=model_path,
+        prompt="### System: "+bot_data["bot_personality"]+chat_history_string+"\n\n### Instructions:\n"+question+"\n\n### Response:\n",
+        max_tokens=128,
+        stop=["###"],
+        repeat_penalty=1.2,
+    )
+
+    return response['choices'][0]["text"]
+
+
+def get_llama_models():
+    models = []
+    for file in os.listdir('./models'):
+        if file.endswith('.bin'):
+            models.append(file)
+
+    return models
+
+
+async def start_up(current_model):
+    # starts up the llama server.
+    command = "python -m llama_cpp.server --model "+current_model
+    subprocess.Popen(command, shell=True)
+
+
+def change_model(model):
+    # kill the old llama server
+    subprocess.run(['npx', 'kill-port', '8000'])
+    # start the llama server.
+    command = "python -m llama_cpp.server --model "+model
+    subprocess.Popen(command, shell=True)
+
+
+def chat_mode(model):
+    chat_history = []
+    while True:
+        question = input("Question: ")
+        response = get_llama_response(question, model, "test", chat_history)
+        chat_history.append({"bot_message": response, "user_message": question})
+        print(response)
+
+
+if __name__ == "__main__":
+    # start_up(r"E:\git\astral-LLM\backend\llama\models\ru-openllama-7b-v5-q5_K.bin")
+    # print(get_llama_response("What kind of animal can meow?", "./models/ru-openllama-7b-v5-q5_K.bin", "test"))
+    # print(get_llama_models())
+    chat_mode(r"E:\git\astral-LLM\backend\llama\models\ru-openllama-7b-v5-q5_K.bin")
